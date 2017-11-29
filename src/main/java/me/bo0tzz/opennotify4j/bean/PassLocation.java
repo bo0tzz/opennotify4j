ad2ff925cb7235d8866ccbffdfa306aeca9d4470
Bo0tzz <btdewilligen@gmail.com>
2017-11-29 14:44:38 +0100

    Fix build

--- src/main/java/me/bo0tzz/opennotify4j/bean/PassLocation.java
+++ src/main/java/me/bo0tzz/opennotify4j/bean/PassLocation.java
@@ -1,22 +1,28 @@
 package me.bo0tzz.opennotify4j.bean;
 
 import lombok.Builder;
+import lombok.EqualsAndHashCode;
 import lombok.Value;
 
 @Value
-@Builder
+@EqualsAndHashCode(callSuper = true)
 public class PassLocation extends Location {
 
     Float altitude;
     Integer passes;
     Long datetime;
 
-    private PassLocation(Float latitude, Float longitude, Float altitude, Integer passes, Long datetime) {
+    @Builder
+    protected PassLocation(Float latitude, Float longitude, Float altitude, Integer passes, Long datetime) {
         super(latitude, longitude);
         if (altitude < 0.0 || altitude > 10000.0) throw new IllegalArgumentException("Altitude must be between 0.0 and 10,000.0!");
         this.altitude = altitude;
         this.passes = passes;
         this.datetime = datetime;
+    }
+
+    static class PassLocationBuilder extends Location.LocationBuilder {
+
     }
 
 }

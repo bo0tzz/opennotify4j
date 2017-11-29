cc0e64a043be06b6a6837e94177c6283c7b19d11
Bo0tzz <btdewilligen@gmail.com>
2017-11-29 15:29:16 +0100

    Add null checks to Location builder

--- src/main/java/me/bo0tzz/opennotify4j/bean/Location.java
+++ src/main/java/me/bo0tzz/opennotify4j/bean/Location.java
@@ -1,6 +1,7 @@
 package me.bo0tzz.opennotify4j.bean;
 
 import lombok.Builder;
+import lombok.NonNull;
 import lombok.Value;
 import lombok.experimental.NonFinal;
 
@@ -12,7 +13,7 @@ public class Location {
     Float longitude;
 
     @Builder
-    protected Location(Float latitude, Float longitude) {
+    protected Location(@NonNull Float latitude, @NonNull Float longitude) {
         if (latitude < -90 || latitude > 90) throw new IllegalArgumentException("Latitude must be between -90.0 and +90.0!");
         if (longitude < -180 || longitude > 180) throw new IllegalArgumentException("Longitude must be between -90.0 and +90.0!");
         this.latitude = latitude;
